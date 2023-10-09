a4804fa3e2f5f297d89fa48d71be0523dd5ff034
Christian Santiago Vera Rojas <104735286+SantiagoVera9812@users.noreply.github.com>
2023-10-08 23:07:05 -0500

    endpoints de Album, Track y Voto terminado

--- src/test/java/com/example/musicalist/VotoControllerTest.java
+++ src/test/java/com/example/musicalist/VotoControllerTest.java
@@ -1,5 +1,7 @@
 package com.example.musicalist;
 
+import javax.transaction.Transactional;
+
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
@@ -11,6 +13,7 @@ import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
 @SpringBootTest
 @AutoConfigureMockMvc
+@Transactional
 public class VotoControllerTest {
 
     @Autowired
